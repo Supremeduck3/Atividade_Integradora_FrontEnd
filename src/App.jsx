a3e058349d62d9a3bac89cd60bc2b4501edf633b
nicolly <saantos.nick1606@gmail.com>
2026-05-04 09:05:10 -0300

    feat: finaliza página integrantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './style/App.css';
 
 const membros = [
-    { nome: 'Maria Eduarda Braga', foto: '/public/img/maria.png' },
+    { nome: 'Maria Eduarda Braga Ferreira', foto: '/public/img/maria.png' },
+    { nome: 'Manuela Maestro', foto: '/public/img/manu.png' },
+    { nome: 'Yasmin Isabele Porto', foto: '/public/img/porto.png' },
+    { nome: 'Nicolly Rodrigues de Souza Santos', foto: '/public/img/nick.png' },
+    { nome: 'Ana Beatriz de Souza Gomes', foto: '/public/img/bea.jpg' },
+    { nome: 'Kaique Alves de Souza', foto: '/public/img/kaique.png' },
+    { nome: 'Vinícius Machado de Souza', foto: '/public/img/viko.png' },
+    { nome: ' Danilo Martins Silva', foto: '/public/img/danilo.png' },
+    { nome: 'Rafaela Simonetti Trajano Silva', foto: '/public/img/rafa.png' },
+    { nome: 'Pedro Moreira Garavalli', foto: '/public/img/pedro.png' },
+    { nome: 'Emilio Henrique dos Santos Favoretto', foto: '/public/img/emilio.png' },
+    { nome: 'João Pedro Rodrigues Stopiglia', foto: '/public/img/joao.png' },
+    {nome:'Pedro Otávio Braga', foto: '/public/img/pedroo.png'}
 ];
 
 function App() {
