aec1cb4bbb90a41060fc9b41b998d27e0768086c
supremeduck008 <pedro.otaviobraga@gmail.com>
2026-05-08 09:28:22 -0300

    cria a parte de tese para receber api externa no react

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/login";
-
+import Teste from "./pages/teste";
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/teste" element={<Teste />} />
             </Routes>
         </BrowserRouter>
     );
