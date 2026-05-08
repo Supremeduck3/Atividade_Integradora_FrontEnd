57460c619068a2bc62879098e94d6fe0b56c03ca
nicolly <saantos.nick1606@gmail.com>
2026-05-08 13:35:00 -0300

    feat: finaliza home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import "../styles/home.module.css";
+import styles from "../style/home.module.css";
+
 import Header from "../components/header";
 import Footer from "../components/footer";
 
@@ -25,3 +26,41 @@ const livros = [
   },
 ];
 
+function Home() {
+    return (
+        <div className={styles.container}>
+            <Header />
+
+            <section className={styles.hero}>
+                <img src="/public/img/icones.png" alt='icones' />
+
+                <h1>
+                    Estude <span>livros</span> de forma inteligente
+                </h1>
+
+                <p>
+                    Explore resumos, análises e conteúdos para estudar melhor.
+                </p>
+
+                <button>Começar Agora</button>
+            </section>
+
+            <section className={styles.booksSection}>
+                <h2>Explore os livros disponíveis</h2>
+
+                <div className={styles.booksGrid}>
+                    {livros.map((livro, index) => (
+                        <div key={index} className={styles.card}>
+                            <img src={livro.capa} alt={livro.titulo} />
+                            <h3>{livro.titulo}</h3>
+                            <p>{livro.autor}</p>
+                        </div>))}
+                </div>
+            </section>
+
+            <Footer />
+            </div>
+    );
+}
+
+export default Home;
