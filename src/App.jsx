8f01506e0b6fb25f15395f51e264982b264b1977
Henrico <maria.e.ferreira77@aluno.senai.br>
2026-05-11 11:43:48 -0300

    feat: sobe as páginas personagens e detalhes personagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Login from "./pages/login";
-import Teste from "./pages/teste";
-import Developers from "./pages/Developers"
+import Developers from "./pages/Developers";
+import PaginaPersonagens from "./pages/PaginaPersonagens";
+import DetalhePersonagem from "./pages/DetalhePersonagem";
+
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/dev" element={<Developers />} />
-                <Route path= "*" element={<notFound/>} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/personagens" element={<PaginaPersonagens />} />
+        <Route path="/personagem/:id" element={<DetalhePersonagem />} />
+        <Route path="/dev" element={<Developers />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
