da191881fbad20f132fc7958e5f36dd4bfba92fc
Supremeduck008 <pedro.otaviobraga@gmail.com>
2026-06-01 10:34:24 -0300

    fix: concerta o carregamento da header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,39 +1,9 @@
-import { useState, useEffect } from 'react';
 import styles from './header.module.css';
 import Carregamento from '../carregamento/carregamento';
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 export default function Header() {
-    const [dados, setDados] = useState(null);
-    const [erro, setErro] = useState(null);
+    const logado = true;
 
-    const logado = true
-    useEffect(() => {
-        async function carregarDados() {
-            try {
-                const api = await fetch(
-                    'https://atividade-portugues-backend.onrender.com/api/upload/2/imagem',
-                    {
-                        headers: {
-                            'x-api-key': 'chaveSecreta',
-                        },
-                    },
-                );
-                const json = await api.json();
-
-                setDados(json);
-            } catch (e) {
-                setErro('Erro ao carregar os dados da equipe.');
-                console.error(e);
-            }
-        }
-
-        carregarDados();
-    }, []);
-
-    if (erro) return <div>{erro}</div>;
-    if (!dados) return <Carregamento/>
-
-    const img = dados.url;
     return (
         <header className={styles.header}>
             <h1 className={styles.logo}>Bookly</h1>
@@ -72,12 +42,15 @@ export default function Header() {
                     Personagens
                 </NavLink>
 
-                <NavLink to="/sobre" className={({ isActive }) =>
-            isActive ? styles['pagina_ativa'] : styles['pagina_desativa']}>
-                   Sobre
+                <NavLink
+                    to="/sobre"
+                    className={({ isActive }) =>
+                        isActive ? styles['pagina_ativa'] : styles['pagina_desativa']
+                    }>
+                    Sobre
                 </NavLink>
                 <NavLink to="/login" className={styles.signin_btn}>
-                   Login
+                    Login
                 </NavLink>
             </nav>
         </header>
